9f3e31c60581b66691e788f4121067695b36fb6b
Hrafven <hrafven@gmail.com>
2021-06-16 09:56:52 +0000

    Rename app.py to sprm.py

--- sprm.py
+++ sprm.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
 import glob
+import argparse
 import re
 import os
 from concurrent import futures
 from xml.etree.ElementTree import parse
+from PathType import PathType
 
 regexp = re.compile(r'[A-z]+_{2}.*')
 
@@ -16,8 +19,7 @@ def process_file(filename):
     # with open(filename, 'r', encoding="utf-8") as content:
 
 
-if __name__ == '__main__':
-    list_of_files = list(glob.iglob('folder/**/*.xml', recursive=True))
+def run(list_of_files):
     with futures.ThreadPoolExecutor(max_workers=5) as executor:
         future_to_file = dict((executor.submit(process_file, filename), filename)
                               for filename in list_of_files)
@@ -31,3 +33,16 @@ if __name__ == '__main__':
             filename, is_deleted = future.result()
             if is_deleted:
                 print('%s deleted' % filename)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Delete managed package metadata files and references')
+    parser.add_argument('directory', type=PathType(exists=True, type='dir'),
+                        help='directory to process')
+
+    args = parser.parse_args()
+
+    list_of_files = list(glob.iglob('%s/**/*.xml' %
+                                    args.directory, recursive=True))
+    run(list_of_files)
